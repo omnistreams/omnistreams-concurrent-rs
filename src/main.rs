33d9e63061804724dbf3c9fec3f4e8381c46d026
Anders Pitman <tapitman11@gmail.com>
2019-01-10 17:11:43 -0700

    Use boxed closures to avoid generics

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,104 @@
 mod omnistreams_concurrent;
 
 use omnistreams_core::{Producer};
-use std::thread;
-use std::sync::mpsc::{Sender, Receiver};
-use std::sync::mpsc;
 use serde_json;
 use serde_json::Value;
-use ws::{listen, Message};
+use ws::{listen, Handler, Sender, Result, Message, CloseCode};
+
+use std::thread;
+use std::sync::mpsc::{Sender as ChannelSender, Receiver as ChannelReceiver};
+use std::sync::mpsc;
+
+struct Server {
+    out: Sender,
+    tx: ChannelSender<Vec<u8>>,
+}
+
+impl Handler for Server {
+
+    fn on_message(&mut self, msg: Message) -> Result<()> {
+        // Echo the message back
+        self.out.send(msg)
+    }
+
+    fn on_close(&mut self, code: CloseCode, reason: &str) {
+        // The WebSocket protocol allows for a utf8 reason for the closing state after the
+        // close code. WS-RS will attempt to interpret this data as a utf8 description of the
+        // reason for closing the connection. I many cases, `reason` will be an empty string.
+        // So, you may not normally want to display `reason` to the user,
+        // but let's assume that we know that `reason` is human-readable.
+        match code {
+            CloseCode::Normal => println!("The client is done with the connection."),
+            CloseCode::Away   => println!("The client is leaving the site."),
+            _ => println!("The client encountered an error: {}", reason),
+        }
+    }
+}
+
+fn main() {
+    listen("127.0.0.1:9001", |out| {
+    println!("Create mux");
+
+    let mut mux = omnistreams_concurrent::Multiplexer::new();
+
+    let (tx, rx): (ChannelSender<Vec<u8>>, ChannelReceiver<Vec<u8>>) = mpsc::channel();
+
+    let sender = out.clone();
+
+    thread::spawn(move || {
+
+        mux.on_conduit(|mut producer, metadata| {
+            producer.on_data(|data| {
+                println!("got data: {:?}", data);
+            });
+        });
+
+        mux.set_send_handler(move |msg| {
+            sender.send(msg).unwrap();
+        });
+
+        for msg in rx {
+            mux.handle_message(&msg);
+        }
+    });
+
+    move |msg| {
+
+        match msg {
+            Message::Text(_) => {
+                println!("WARNING: text message received. unhandled");
+            },
+            Message::Binary(v) => {
+                tx.send(v).unwrap();
+            },
+        }
+
+        Ok(())
+    }
+
+  }).unwrap()
+} 
+/*
+mod omnistreams_concurrent;
+
+use omnistreams_core::{Producer};
+//use std::thread;
+//use std::sync::mpsc::{Sender, Receiver};
+//use std::sync::mpsc;
+use serde_json;
+use serde_json::Value;
+use ws::{listen, Handler, Message, Sender};
+
+struct Server {
+    out: Sender,
+}
+
+impl Handler for Server {
+    fn on_message(&mut self, msg: Message) -> Result<()> {
+        // Echo the message back
+        self.out.send(msg)
+    }
+}
 
 fn main() {
 
@@ -31,12 +123,12 @@ fn main() {
             stream.request(5);
         });
 
-        let (message_sender, message_receiver): (Sender<Vec<u8>>, Receiver<Vec<u8>>) = mpsc::channel();
-        thread::spawn(move || {
-            for data in message_receiver {
-                mux.handle_message(&data);
-            }
-        });
+        //let (message_sender, message_receiver): (Sender<Vec<u8>>, Receiver<Vec<u8>>) = mpsc::channel();
+        //thread::spawn(move || {
+        //    for data in message_receiver {
+        //        mux.handle_message(&data);
+        //    }
+        //});
 
         move |msg| {
 
@@ -45,7 +137,7 @@ fn main() {
                     println!("WARNING: text message received. unhandled");
                 },
                 Message::Binary(v) => {
-                    message_sender.send(v).unwrap();
+                    //message_sender.send(v).unwrap();
                 },
             }
 
@@ -53,3 +145,4 @@ fn main() {
         }
     }).unwrap()
 }
+*/
--- src/omnistreams_concurrent.rs
+++ src/omnistreams_concurrent.rs
@@ -8,26 +8,25 @@ const MESSAGE_TYPE_CONTROL_MESSAGE: u8 = 5;
 use std::collections::HashMap;
 use omnistreams_core::{Producer};
 
-
-pub struct Multiplexer<T, U, V>
-    where T: Fn(&[u8]), U: Fn(&[u8]), V: Fn(&mut ReceiveStream<U>, &[u8])
-{
-    send: T,
-    receive_streams: HashMap<u8, ReceiveStream<U>>,
-    on_stream: V,
+pub struct Multiplexer {
+    send: Box<Fn(&[u8]) + Send>,
+    conduit_callback: Box<Fn(&mut ReceiveStream, &[u8]) + Send>,
     next_stream_id: u8,
+    receive_streams: HashMap<u8, ReceiveStream>,
 }
 
-impl<T, U, V> Multiplexer<T, U, V>
-    where T: Fn(&[u8]), U: Fn(&[u8]), V: Fn(&mut ReceiveStream<U>, &[u8])
-{
-    pub fn new(send: T, on_stream: V) -> Multiplexer<T, U, V> {
+impl Multiplexer {
+    pub fn new() -> Multiplexer {
         Multiplexer {
-            send,
-            on_stream,
-            receive_streams: HashMap::new(),
+            send: Box::new(|msg| {}),
+            conduit_callback: Box::new(|mut producer, metadata| {}),
             next_stream_id: 0,
+            receive_streams: HashMap::new(),
         }
+    }
+
+    pub fn set_send_handler<C: 'static + Fn(&[u8]) + Send>(&mut self, callback: C) {
+        self.send = Box::new(callback);
     }
 
     pub fn handle_message(&mut self, msg: &[u8]) {
@@ -39,21 +38,24 @@ impl<T, U, V> Multiplexer<T, U, V>
         match message_type {
             MESSAGE_TYPE_CREATE_RECEIVE_STREAM => {
                 println!("create stream {}: {:?}", stream_id, data);
+
                 let request = |num_items| {
-                    println!("request called: {}", num_items);
+                    //println!("request called: {}", );
+                    //(self.send)(&[1,2,3])
                 };
 
-                let mut stream = ReceiveStream::new(request);
+                let mut producer = ReceiveStream {
+                    data_callback: Box::new(|msg| {}),
+                    upstream_request: Box::new(request),
+                };
                 let stream_id = self.next_stream_id;
                 self.next_stream_id += 1;
-                (self.on_stream)(&mut stream, data);
-                self.receive_streams.insert(stream_id, stream);
+                (self.conduit_callback)(&mut producer, data);
+                self.receive_streams.insert(stream_id, producer);
             },
             MESSAGE_TYPE_STREAM_DATA => {
                 if let Some(producer) = self.receive_streams.get(&stream_id) {
-                    if let Some(callback) = &producer.data_callback {
-                        callback(data);
-                    }
+                    (producer.data_callback)(data);
                 }
             },
             MESSAGE_TYPE_STREAM_END => {
@@ -66,39 +68,114 @@ impl<T, U, V> Multiplexer<T, U, V>
             },
         }
     }
-}
 
-#[derive(Debug)]
-pub struct ReceiveStream<V>
-    where V: Fn(&[u8])
-{
-    data_callback: Option<V>,
-    upstream_request: fn(u8),
-}
-
-impl<V> ReceiveStream<V>
-    where V: Fn(&[u8])
-{
-    pub fn new(upstream_request: fn(u8)) -> ReceiveStream<V> {
-
-        ReceiveStream {
-            data_callback: None,
-            upstream_request,
-        }
+    pub fn on_conduit<C: 'static + Fn(&mut ReceiveStream, &[u8]) + Send>(&mut self, callback: C) {
+        self.conduit_callback = Box::new(callback);
     }
 }
 
-impl<V> Producer<V> for ReceiveStream<V>
-    where V: Fn(&[u8])
-{
-    fn on_data(&mut self, callback: V) {
-        self.data_callback = Some(callback);
-    }
+pub struct ReceiveStream {
+    data_callback: Box<Fn(&[u8]) + Send>,
+    upstream_request: Box<Fn(u8) + Send>,
+}
 
-    fn request(&self, num_items: u8) {
-        (self.upstream_request)(num_items);
+impl ReceiveStream {
+    pub fn on_data<C: 'static + Fn(&[u8]) + Send>(&mut self, callback: C) {
+        self.data_callback = Box::new(callback);
     }
 }
+
+
+//pub struct Multiplexer<T, U, V>
+//    where T: Fn(&[u8]), U: Fn(&[u8]), V: Fn(&mut ReceiveStream<U>, &[u8])
+//{
+//    send: T,
+//    receive_streams: HashMap<u8, ReceiveStream<U>>,
+//    on_stream: V,
+//    next_stream_id: u8,
+//}
+//
+//impl<T, U, V> Multiplexer<T, U, V>
+//    where T: Fn(&[u8]), U: Fn(&[u8]), V: Fn(&mut ReceiveStream<U>, &[u8])
+//{
+//    pub fn new(send: T, on_stream: V) -> Multiplexer<T, U, V> {
+//        Multiplexer {
+//            send,
+//            on_stream,
+//            receive_streams: HashMap::new(),
+//            next_stream_id: 0,
+//        }
+//    }
+//
+//    pub fn handle_message(&mut self, msg: &[u8]) {
+//
+//        let message_type = msg[0];
+//        let stream_id = msg[1];
+//        let data = &msg[2..];
+//
+//        match message_type {
+//            MESSAGE_TYPE_CREATE_RECEIVE_STREAM => {
+//                println!("create stream {}: {:?}", stream_id, data);
+//                let request = |num_items| {
+//                    println!("request called: {}", num_items);
+//                };
+//
+//                let mut stream = ReceiveStream::new(request);
+//                let stream_id = self.next_stream_id;
+//                self.next_stream_id += 1;
+//                (self.on_stream)(&mut stream, data);
+//                self.receive_streams.insert(stream_id, stream);
+//            },
+//            MESSAGE_TYPE_STREAM_DATA => {
+//                if let Some(producer) = self.receive_streams.get(&stream_id) {
+//                    if let Some(callback) = &producer.data_callback {
+//                        callback(data);
+//                    }
+//                }
+//            },
+//            MESSAGE_TYPE_STREAM_END => {
+//            },
+//            MESSAGE_TYPE_TERMINATE_SEND_STREAM=> {
+//            },
+//            MESSAGE_TYPE_STREAM_ACK => {
+//            },
+//            _ => {
+//            },
+//        }
+//    }
+//}
+//
+//#[derive(Debug)]
+//pub struct ReceiveStream<V>
+//    where V: Fn(&[u8])
+//{
+//    data_callback: Option<V>,
+//    upstream_request: fn(u8),
+//}
+//
+//impl<V> ReceiveStream<V>
+//    where V: Fn(&[u8])
+//{
+//    pub fn new(upstream_request: fn(u8)) -> ReceiveStream<V> {
+//
+//        ReceiveStream {
+//            data_callback: None,
+//            upstream_request,
+//        }
+//    }
+//}
+//
+//impl<V> Producer<V> for ReceiveStream<V>
+//    where V: Fn(&[u8])
+//{
+//    fn on_data(&mut self, callback: V) {
+//        self.data_callback = Some(callback);
+//    }
+//
+//    fn request(&self, num_items: u8) {
+//        (self.upstream_request)(num_items);
+//    }
+//}
 
 
 #[cfg(test)]
